8ea8b671c09d23562bbe7159bd6d24a1d13d668a
Tyler <malonet@g.cofc.edu>
2020-03-02 17:30:54 -0500

    removed unnecessary import

--- Project2.py
+++ Project2.py
@@ -2,7 +2,7 @@ from __future__ import print_function
 import sys
 import os
 import random
-import time
+
 
     ####################### ISSUES ###########################################################
 
